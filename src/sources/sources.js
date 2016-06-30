ca86cd0e8d73f46d21fc6690b181271a702c78fc
unknown <raphealguo@qq.com>
2016-06-30 16:23:28 +0800

    add vconsole-source

---
+++ src/sources/sources.js
@@ -0,0 +1,122 @@
+import tplSource from './sources.html';
+import './sources.less';
+
+let $ = vConsole.$;
+
+class VConsoleSourcesTab extends vConsole.VConsolePlugin {
+  constructor(...args) {
+    super(...args);
+    this.resUrls = VConsoleSourcesTab.getAllResUrl();
+
+  }
+  onRenderTab(callback) {
+    var that = this;
+    var resUrls = this.resUrls;
+    var $dom = this.$dom = $.render(tplSource, {resUrls:resUrls})
+
+    callback($dom);
+
+    $.bind($.all('.vc-sources-file', $dom), 'click', function(){
+      $.removeClass($.all('.vc-sources-file', $dom), "actived");
+      $.addClass(this, "actived");
+      let idx = this.dataset.idx*1, res = resUrls[idx];
+
+      if (!!res){
+        $.one(".vc-sources-url", $dom).innerText = res.url;
+        if (res.content){
+          let $content = $.one(".vc-sources-content", $dom);
+          let $iframeContent = $.one(".vc-sources-iframe-content", $dom);
+          $content.style.display = "block";
+          $iframeContent.style.display = "none";
+          $content.innerText = res.content;
+        }else{
+          that.fillContent(res.url, idx);
+        }
+      }
+    });
+  }
+
+  static getAllResUrl(){
+    var resList = [];
+
+    resList.push({url:location.href, filename:VConsoleSourcesTab.getFileName(location.href), type:"html", content:document.getElementsByTagName("html")[0].innerHTML});
+    var otherRes = $.all('link[rel="stylesheet"],script[src]');
+    for (let res of otherRes) {
+      let url = "";
+      let filename = "";
+      let type = "unknow";
+      let nodeName = res.nodeName.toUpperCase();
+      if (nodeName == "LINK"){ 
+        url = res.href || "";
+        type = "css";
+      }else if (nodeName == "SCRIPT"){
+        url = res.src || "";
+        type = "script";
+      }
+      filename = VConsoleSourcesTab.getFileName(url);
+      if (!!filename){
+        resList.push({url:url, filename:filename, type:type});
+      }
+    }
+    return resList;
+  }
+
+  static getFileName(url){
+    var reg = /\/([^\/\?]+)($|\?)/g;
+    var match = reg.exec(url);
+    return match && match[1];
+  }
+
+  getResContent(url, callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = ()=>{
+      if (xhr.readyState == 3) {
+      }
+      if (xhr.readyState == 4) {
+        xhr.onreadystatechange = null;
+        var status = xhr.status;
+        if ( status >= 200 && status < 400 ) {
+          callback(xhr.responseText);
+        } else {
+          callback("[ERROR] Please Try Again.");
+        }
+      }
+    }
+    xhr.send();
+  }
+  fillContent(url, idx){
+    var urlSuf = location.protocol + "//" + location.host;
+    var $dom = this.$dom;
+    var $content = $.one(".vc-sources-content", $dom);
+    var $iframeContent = $.one(".vc-sources-iframe-content", $dom);
+    var resUrls = this.resUrls;
+    if (url.indexOf(urlSuf) == 0){
+      $content.innerText = "loading...";
+      $content.style.display = "block";
+      $iframeContent.style.display = "none";
+      this.getResContent(url, (text)=>{
+        $content.innerText = text;
+        resUrls[idx] && (resUrls[idx].content = text);
+      });
+    }else{
+      //iframe
+      $iframeContent.src = url;
+      $content.style.display = "none";
+      $iframeContent.style.display = "block";
+    }
+  }
+}
+
+/*
+var sourcePlugin = new vConsole.VConsolePlugin('vconsole-source', 'Source');
+
+sourcePlugin.on('renderTab', function(callback) {
+  var html = '<div>Click the tool button below!</div>';
+  callback(html);
+});
+
+vConsole.addPlugin(sourcePlugin);
+*/
+
+vConsole.addPlugin(new VConsoleSourcesTab('vconsole-source', 'Source'));
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var pkg = require('./package.json');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: false,
+  entry: {
+    sources : './src/sources/sources.js'
+  },
+  output: {
+    path: './dist',
+    filename: 'vconsole-[name].min.js',
+    library: 'vConsole-sources',
+    libraryTarget: 'umd',
+    umdNameDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/, loader: 'html'
+      },
+      { 
+        test: /\.js$/, loader: 'babel'
+      },
+      {
+        test: /\.less$/,
+        loader: 'style!css!less'
+        // loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader') // 将css独立打包
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin([
+        pkg.name + ' v' + pkg.version + ' (' + pkg.homepage + ')',
+        'Copyright ' + new Date().getFullYear() + ', ' + pkg.author,
+        pkg.license +' license'
+    ].join('\n'))
+    /*
+    ,new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })*/
+    // ,new ExtractTextPlugin('[name].min.css') // 将css独立打包
+  ]
+
+};
